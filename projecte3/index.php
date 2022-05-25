e927313080473d3d0c609bb87f7a0db8dc8a78a8
aleixbafl <80314325+aleixbafl@users.noreply.github.com>
2022-05-25 12:21:02 +0200

    Add files via upload

---
+++ projecte3/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta http-equiv="X-UA_Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/general.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="shortcut icon" href="img/logos/logo4.png">
+    <title>Projecte 3</title>
+  </head>
+  <body>
+    <header>
+      <img src="img/logos/logo4.png" alt="Pirineus DAM" id="logo_header">
+      <h1>Pirineus DAM</h1>
+    </header>
+
+    <nav>
+      <div id="menu" class="nav nav-pills flex-column flex-sm-row">
+        <a class="flex-sm-fill text-sm-center nav-link active" href=".">Inici</a>
+        <a class="flex-sm-fill text-sm-center nav-link" href="productes/inici.php">Productes</a>
+        <a class="flex-sm-fill text-sm-center nav-link" href="#">Carro</a>
+        <a class="flex-sm-fill text-sm-center nav-link" href="#">Compte</a>
+      </div>
+    </nav>
+
+    <main>
+      <article class="">
+        <section>
+          <h2 class="h2_index">Pàgina d'Inici</h2>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+          <p>Contingut de la pàgina d'inici.</p>
+        </section>
+      </article>
+
+    </main>
+  </body>
+</html>
---
+++ projecte3/productes/inici.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta http-equiv="X-UA_Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../css/general.css">
+    <link rel="stylesheet" href="../css/productes.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../img/logos/logo4.png">
+    <title>Productes de Pirineus DAM</title>
+  </head>
+  <body>
+    <header>
+      <img src="../img/logos/logo4.png" alt="Pirineus DAM" id="logo_header">
+      <h1>Pirineus DAM</h1>
+    </header>
+
+    <nav>
+      <div id="menu" class="nav nav-pills flex-column flex-sm-row">
+        <a class="flex-sm-fill text-sm-center nav-link" href="..">Inici</a>
+        <a class="flex-sm-fill text-sm-center nav-link active" href="#">Productes</a>
+        <a class="flex-sm-fill text-sm-center nav-link" href="#">Carro</a>
+        <a class="flex-sm-fill text-sm-center nav-link" href="#">Compte</a>
+      </div>
+    </nav>
+
+    <main id="productes">
+      <h2>Esquís</h2>
+      <article class="productes1">
+        <section class="productes2">
+          <div class="card">
+            <img src="../img/esquis/1.png" class="card-img-top" alt="producte1">
+            <div class="card-body">
+              <h5 class="card-title">Esquís 1</h5>
+              <p class="card-text">Descripcio</p>
+              <h6>9.99€</h6>
+              <a href="#" class="btn btn-primary">Afegir al Carro</a>
+            </div>
+          </div>
+          <div class="card">
+            <img src="../img/esquis/2.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Esquís 2</h5>
+              <p class="card-text">Descripcio</p>
+              <h6>9.99€</h6>
+              <a href="#" class="btn btn-primary">Afegir al Carro</a>
+            </div>
+          </div>
+          <div class="card">
+            <img src="../img/esquis/2.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Esquís 3</h5>
+              <p class="card-text">Descripcio</p>
+              <h6>9.99€</h6>
+              <a href="#" class="btn btn-primary">Afegir al Carro</a>
+            </div>
+          </div>
+        </section>
+      </article>
+      <h2>Pals de esquí</h2>
+      <article class="productes1">
+        <section class="productes2">
+          <div class="card">
+            <img src="../img/pals/1.png" class="card-img-top" alt="producte1">
+            <div class="card-body">
+              <h5 class="card-title">Pals 1</h5>
+              <p class="card-text">Descripcio</p>
+              <h6>9.99€</h6>
+              <a href="#" class="btn btn-primary">Afegir al Carro</a>
+            </div>
+          </div>
+          <div class="card">
+            <img src="../img/pals/2.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Pals 2</h5>
+              <p class="card-text">Descripcio</p>
+              <h6>9.99€</h6>
+              <a href="#" class="btn btn-primary">Afegir al Carro</a>
+            </div>
+          </div>
+          <div class="card">
+            <img src="../img/pals/3.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Pals 3</h5>
+              <p class="card-text">Descripcio</p>
+              <h6>9.99€</h6>
+              <a href="#" class="btn btn-primary">Afegir al Carro</a>
+            </div>
+          </div>
+        </section>
+      </article>
+    </main>
+  </body>
+</html>
